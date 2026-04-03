e37fd6a144604f71b9125cc2ba3d09ff6573934f
Link4Electronics <link4electronics@gmail.com>
2026-04-03 01:59:16 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+    libsidplayfp   \
     pipewire-audio \
     pipewire-jack
 
